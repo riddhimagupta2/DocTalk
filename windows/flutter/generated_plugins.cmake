85ab02dd22af10bb4c0aec9ac4aaa899ddd56643
riddhima <riddhimag226@gmail.com>
2026-03-21 19:40:21 +0530

    commit

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,14 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_windows
   cloud_firestore
   firebase_auth
   firebase_core
+  flutter_tts
   geolocator_windows
   permission_handler_windows
+  record_windows
   speech_to_text_windows
   url_launcher_windows
 )
